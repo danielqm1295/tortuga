bd4bba56c13263fc4eb8cca4d2ac14c97c7a6b5c
Mario <dvw63274@educastur.es>
2022-03-06 14:55:53 +0100

    Cambios Mario

--- punto_linea.php
+++ punto_linea.php
@@ -1,44 +1,78 @@
 <?php
-    class Punto extends Grafico{ //Hereda de gráfico, o qué.
-        private int $x;
-        private int $y;
+class Punto extends Grafico
+{ //Hereda de gráfico, o qué.
+    private int $x;
+    private int $y;
 
-        public function getX(){
-            return $this->x;
-        }
-
-        public function setX(){
-            $this->x = $x;
-        }
-
-        public function getY(){
-            return $this->y;
-        }
-
-        public function setY(){
-            $this->y = $y;
-        }
-
-        public function dibujar(){
-            return ""; //¿Aquí no recibe nada, no?
-        }
-
-        public function mover($x, $y){
-            $this->x = $x;
-            $this->y = $y;
-        }
-
-        public function desplazar($dx, $dy){
-            $this->x += $dx;
-            $this->y += $dy
-        }
+    public function getX()
+    {
+        return $this->x;
     }
 
-    class Linea extends Punto { //Aquí tendo dudas también, pero viendo el diagrama creo que hereda de Punto.
-        private Punto $centro;
-        
-        public function dibujar(){
-            return "";
-        }
+    public function setX($x)
+    {
+        $this->x = $x;
     }
-?>
+
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    public function setY($y)
+    {
+        $this->y = $y;
+    }
+
+    public function dibujar()
+    {
+        return ""; //¿Aquí no recibe nada, no?
+    }
+
+    public function mover($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function desplazar($dx, $dy)
+    {
+        $this->x += $dx;
+        $this->y += $dy;
+    }
+
+    //Pensar si crear otra clase intermedia entre grafico y linea que tenga lo siguiente
+    public function insertarPunto(Punto $p)
+    {
+        $this->vertices[] = $p;
+        $this->numeroVertices++;
+    }
+    public function getVertice($n)
+    {
+        $p = null;
+        if (count($this->vertices) > $n) {
+            $p = $this->vertices[$n];
+        }
+        return $p;
+    }
+}
+
+class Linea extends Grafico
+{ //Aquí tendo dudas también, pero viendo el diagrama creo que hereda de Punto.
+    private Punto $centro;
+
+    public function __construct($x1, $x2, $y1, $y2)
+    {
+        $p = new Punto($x1, $y1);
+        $p->mover($x1, $y1);
+        $this->centro->insertarPunto($p);
+        $p2 = new Punto($x2, $y2);
+        $p2->mover($x2, $y2);
+        $this->centro->insertarPunto($p2);
+    }
+
+    public function dibujar()
+    {
+        return "<line x1='" . $this->centro->getVertice(0)->getX() . "' y1='" . $this->centro->getVertice(0)->getY() . "' x2='". $this->centro->getVertice(1)->getX()."' y2='".$this->centro->getVertice(1)->getY()."' style='stroke:#f00; stroke-width:15; stroke-linecap: butt'></line>";
+    }
+}
---
+++ tortuga.php
@@ -0,0 +1,35 @@
+<?php
+class tortuga extends Grafico
+{
+    private Punto $centro;
+    private int $angulo;
+
+    public function __construct($x, $y, $angulo)
+    {
+        $this->centro = new Punto($x, $y);
+        $this->centro->mover($x, $y);
+        $this->angulo = $angulo;
+    }
+    public function getCentro()
+    {
+        return $this->centro;
+    }
+    public function setCentro(Punto $p)
+    {
+        $this->centro = $p;
+    }
+    public function mover($x, $y)
+    {
+        $this->centro->mover($x, $y);
+    }
+    public function desplazar($dx, $dy)
+    {
+        $this->centro->desplazar($dx, $dy);
+    }
+    public function girar($angulo){
+        $this->angulo = $angulo;
+    }
+    public function getAngulo(){
+        return $this->angulo;
+    }
+}
