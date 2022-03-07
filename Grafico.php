5720724630660c571238be8e90fe1ef723366b68
dani <danielqm1295@gmail.com>
2022-03-07 11:23:51 +0100

    Creada clase Grafico

---
+++ Grafico.php
@@ -0,0 +1,24 @@
+<?php
+    class Grafico {
+        protected $visible;
+        
+        public function mostrar() {
+            $this ->visible = true;
+        }
+
+        public function mostrar() {
+            $this ->visible = false;
+        }
+
+        public function getvisible() {
+            return $this -> visible;
+        }
+
+        public function dibujar() {}
+
+        public function mover($x, $y) {}
+
+        public function desplazar($x, $y) {}
+}
+
+?>
